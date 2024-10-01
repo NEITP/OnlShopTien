c8839684b9a90cd72ae13589bb1a1ba19061e458
dinhquoctien230302 <dinhquoctien230302@gmail.com>
2024-10-01 17:36:04 +0700

    new

---
+++ OnlShopTien.Model/Models/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlShopTien.Model.Models
+{
+    [Table("Menu")]
+    internal class Menu
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public int? DisplayOrders { get; set; }
+
+        [Required]
+        public int GroupID { get; set; }
+        [ForeignKey ("GroupID")]
+        public virtual MenuGroup MenuGroup { get; set; }
+        public string Target { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ OnlShopTien.Model/Models/MenuGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlShopTien.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
